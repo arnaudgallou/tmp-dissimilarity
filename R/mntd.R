b9659abde5bb21ac858acdd565eb3203ba01cef4
arnaudgallou <arnaudgallou31@gmail.com>
2023-01-10 16:12:13 +0100

    `mntd.R`

---
+++ R/mntd.R
@@ -0,0 +1,97 @@
+####  Helpers  ####
+  {
+    prune <- function(x, y) {
+      UseMethod("prune")
+    }
+
+    prune.phylo <- function(x, y) {
+      keep.tip(x, y)
+    }
+
+    prune.dgCMatrix <- function(x, y) {
+      x[, colnames(x) %in% y]
+    }
+
+    drop_absent <- function(x) {
+      UseMethod("drop_absent")
+    }
+
+    drop_absent.dgCMatrix <- function(x) {
+      x[, Matrix::colSums(x) > 0]
+    }
+
+    make_distance_df <- function(data, tree, pairs, id, drop_absent = TRUE) {
+      apply(pairs, 2, function(i) {
+        list(
+          item = i[1],
+          item_2 = i[2],
+          id = id,
+          distance = calc_mntd(data[i, ], tree, drop_absent)
+        )
+      })
+    }
+
+    equalize <- function(x, tree, drop_absent) {
+      if (drop_absent) {
+        x <- drop_absent(x)
+      }
+      # cat("Community matrix:\n")
+      # print(x)
+      # cat("\n")
+      tip_labels <- tree$tip.label
+      taxa <- tip_labels[tip_labels %in% colnames(x)]
+      list(
+        matrix = prune(x, taxa),
+        tree = prune(tree, taxa)
+      )
+    }
+
+    get_communities <- function(x) {
+      lapply(seq(nrow(x)), function (i) {
+        colnames(x)[x[i, ] == 1]
+      })
+    }
+
+    mntd <- function(x) {
+      # cat("Distance matrix:\n")
+      # print(x)
+      # cat("\n")
+      out <- lapply(1:2, function(i) {
+        apply(x, i, min)
+      })
+      mean(unlist(out))
+    }
+
+    calc_mntd <- function(x, tree, drop_absent) {
+      data <- equalize(x, tree, drop_absent)
+      communities <- get_communities(data$matrix)
+      distances <- ape::cophenetic.phylo(data$tree)
+      out <- distances[communities[[1]], communities[[2]], drop = FALSE]
+      if (length(out) == 1L) {
+        return(as.numeric(out))
+      }
+      mntd(out)
+    }
+  }
+
+####  Influence of the number of taxa in matrix on MNTD  ####
+  {
+    ptree <- read.tree("data/tree.txt")
+
+    communities <- mdf %>%
+      get_taxa_tbl("erigeron", "vagus|compositus|clokeyi|coulteri|mancus") %>%
+      extend(n = 4)
+
+    set.seed("011023")
+
+    df <- semi_difference(communities, n = 3)
+
+    smat <- to_sparse_matrix(df, row_names = "grids", col_names = "species")
+    smat <- smat[c(1, 3), ]
+
+    pairs <- combn(rownames(smat), m = 2)
+
+    tibble(data = make_distance_df(smat, ptree, pairs, 0, drop_absent = TRUE)) %>%
+      unnest_wider(col = data) %>%
+      set_names(c("mountain", "mountain_2", "thermal_band", "phylo_dist"))
+  }
